6180dfa2fcd92eeda92d162c0e06e2935aaaa1d4
wonyk <yingkeatwon@gmail.com>
2020-02-25 08:48:01 +0800

    Squashed 'projects/request-header-microservice/' content from commit 7016699
    git-subtree-dir: projects/request-header-microservice
    git-subtree-split: 7016699f837049ad555f44699e821af3abde866c

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+app.set('trust proxy',true);
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", (req, res) => {
+  const ip = req.ip;
+  const language = req.get("Accept-Language");
+  const info = req.get("User-Agent");
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: info
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
